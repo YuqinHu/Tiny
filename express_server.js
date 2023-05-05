13bdd573092ff670797a152fac06462d043c1f37
YuqinHu <h878688736@gmail.com>
2023-05-04 21:44:55 -0700

    url

--- express_server.js
+++ express_server.js
@@ -3,11 +3,23 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let string = '';
+
+  for (let i = 0; i < 6; i++) {
+    string += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+
+  return string;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,9 +30,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let tinyURL = generateRandomString();
+  urlDatabase[tinyURL] = req.body.longURL;
+  res.redirect(`/urls/${tinyURL}`);
 });
 
 app.listen(PORT, () => {
